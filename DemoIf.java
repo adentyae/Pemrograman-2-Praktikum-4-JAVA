7e0f17fee2f22942ddd8139fa87e9bb1ea999fa9
Adentya Elmas Pranawa <36910608+adentyae@users.noreply.github.com>
2018-09-22 23:34:24 +0700

    Add files via upload

---
+++ DemoIf.java
@@ -0,0 +1,10 @@
+public class DemoIf{
+	public static void main(String[] args) {
+		String nim = "171530003";
+
+		if (nim.equals("171530003")){ //khusus String pakai equals
+			System.out.println("Nama Anda Adentya Elmas Pranawa");
+		}
+	}
+}
+
